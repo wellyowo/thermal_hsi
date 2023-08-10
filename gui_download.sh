19325d652b24c20c408a7b672bd0ecc3e324e429
jinglinjackychen <a2396660@gmail.com>
2023-08-10 14:58:55 +0800

    Update: add wget download script

---
+++ gui_download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#download thermal camera gui
+wget ftp://140.113.148.83/arg-projectfile-download/thermal_camera/Spinnaker-selected/Linux/Ubuntu20.04/spinnaker-3.0.0.118-amd64-pkg.tar.gz
+
+#download thermal camera python api
+wget ftp://140.113.148.83/arg-projectfile-download/thermal_camera/Spinnaker-selected/Linux/Ubuntu20.04/python/spinnaker_python-3.0.0.118-cp38-cp38-linux_x86_64.tar.gz
